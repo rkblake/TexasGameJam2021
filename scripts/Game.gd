55f9dc64629a18b3bdb69808b87d30d01e31fa7e
Randall Blake <rkb.kirby@gmail.com>
2021-09-24 23:19:10 -0400

    added physics objects

--- scripts/Game.gd
+++ scripts/Game.gd
@@ -1,4 +1,13 @@
 extends Node2D
 
+onready var options_scene: PackedScene = preload('res://scenes/Options.tscn')
+
 func _ready():
 	pass
+
+
+func _input(event) -> void:
+	if Input.is_action_just_pressed('ui_cancel'):
+		#TODO pause all physics objects
+		var options = options_scene.instance()
+		$GUI.add_child(options)
